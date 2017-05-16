cecb4d9c528639331b7d6cbbea678ca9f12fc32d
JeffChoi <wndgns0614@gmail.com>
2017-05-16 18:19:01 +1200

    To add testing module

--- app/src/main/java/dms/aut/ac/nz/dmsandroid/MainActivity.java
+++ app/src/main/java/dms/aut/ac/nz/dmsandroid/MainActivity.java
@@ -1,13 +1,26 @@
 package dms.aut.ac.nz.dmsandroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttonForMessage;
+    Button buttonForBluetooth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonToGoToMessage(View v){
+        buttonForMessage = (Button)findViewById(R.id.buttonForMessage);
+        //Intent intent = new Intent()
+    }
+
+    public void buttonToGoToBluetooth(View v){
+        buttonForBluetooth = (Button)findViewById(R.id.buttonForBluetooth);
     }
 }
